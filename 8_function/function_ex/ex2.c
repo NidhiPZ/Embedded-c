5ec6fb5feaffbd9d7f02bb372da1bc460ee18db0
NidhiZP <nidhiipatel08@gmail.com>
2024-07-11 17:38:41 +0530

    file edited

--- 8_function/function_ex/ex2.c
+++ 8_function/function_ex/ex2.c
@@ -12,3 +12,14 @@ int main()
     printf("%d ",fun());
     return 0;
 }
+/*
+initialize ;   condition  ;        updation
+func(16) T ;   func(15) T ; -> 14 ; func() / 13
+func(13) T ;   func(12) T ; -> 11 ; func(13) / 10
+func(10) T ;   func(9) T ; -> 8   ; func(10)/7
+func(7) T ;   func(6) T ; -> 5   ; func(7) /4
+func(4) T ;   func(3) T ; -> 2   ; func(4) /1
+func(1) T ;   func(0) F ; -> out of loop; func(1)
+
+ot put will be -> 14,11,8,5,2
+*/
