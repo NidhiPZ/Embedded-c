e231bf33b5d7dac157319caf3450c595db171362
NidhiZP <nidhiipatel08@gmail.com>
2024-07-19 12:50:57 +0530

    file edited

--- 12_Structure/5_nested_structure.c
+++ 12_Structure/5_nested_structure.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
+//we define an "employee" data type with one of its elements being "dob"
 struct employee
 {
    char name[10];
-   float salary;
-   struct dob{
+   int salary;
+//declare dob struct with int types d, m, y inside the employee structure and its variable "d1"
+   struct dob
+   {
       int d, m, y;
    } d1;
 };
@@ -13,9 +15,9 @@ struct employee
 int main()
 {
 
-   struct employee e1 = {"Kiran", 25000, {12, 5, 1990}};
+   struct employee e1 = {"Sanam", 25000, {12, 8, 1999} };
    printf("Name: %s\n", e1.name);
-   printf("Salary: %f\n", e1.salary);
+   printf("Salary: %d\n", e1.salary);
    printf("Date of Birth: %d-%d-%d\n", e1.d1.d, e1.d1.m, e1.d1.y);
    
    return 0;
